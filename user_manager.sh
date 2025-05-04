999dee118ecfcb4e662934d819f65e3feb60fe9f
Fareen Pourmoussavian <34846620+Jormungandr21@users.noreply.github.com>
2025-05-03 23:18:12 -0400

    Squashed commit of the following:
    commit 100608f9e7c5a6b5cc532535739c65c2eb531aec
    Author: Fareen Pourmoussavian <34846620+Jormungandr21@users.noreply.github.com>
    Date:   Sat May 3 23:17:45 2025 -0400
    
        delete method

--- user_manager.sh
+++ user_manager.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-
+logger="./user_manager.log"
 create_user() { 
     local username=$1
     local password=$2
 
     sudo useradd "$username"
 
-    echo "User '$username' has been created"
+    echo "User '$username' has been created" >> "$logger"
 }
+
+delete_user(){
+    local username=$1   
+    if id "$username" &>/dev/null; then
+    sudo userdel "$username"
+    echo "INFO: User $username deleted successfully." >> "$logger"
+  else
+    echo "ERROR: User $username does not exist." >> "$logger"
+  fi
+}
+delete_user "userTest2"
+create_user "userTest2"
+delete_user "userTest2"
